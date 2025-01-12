419d922cf056f7465e75549deee6239f1c6d28ef
Johannes Förstner <johannes@famfoerstner.de>
2025-01-12 19:32:35 +0100

    Modernize the repo scripts

--- build.bat
+++ build.bat
@@ -1,27 +1,23 @@
-:: This script is supposed to be executed from your DS installation folder.
-:: TankCreator and gaspy are expected to be in sibling dirs.
-
 :: name of map
 set map=kingdom-of-ehb
 :: name of map, case-sensitive
 set map_cs=Kingdom of Ehb
-:: path of DSLOA documents dir (where Bits are)
-set doc_dsloa=%USERPROFILE%\Documents\Dungeon Siege LoA
+
+:: path of Bits dir
+set bits=%~dp0.
 :: path of DS installation
-set ds=.
+set ds=%DungeonSiege%
 :: path of TankCreator
-set tc=..\TankCreator
-:: path of GasPy
-set gaspy=..\gaspy
+set tc=%TankCreator%
 
 :: Compile map file
 rmdir /S /Q "%tmp%\Bits"
-robocopy "%doc_dsloa%\Bits\world\maps\%map%" "%tmp%\Bits\world\maps\%map%" /E
+robocopy "%bits%\world\maps\%map%" "%tmp%\Bits\world\maps\%map%" /E
 pushd %gaspy%
-venv\Scripts\python -m build.fix_start_positions_required_levels %map% "%tmp%\Bits"
+venv\Scripts\python -m build.fix_start_positions_required_levels %map% --bits "%tmp%\Bits"
 if %errorlevel% neq 0 pause
 popd
-%tc%\RTC.exe -source "%tmp%\Bits" -out "%ds%\Maps\%map_cs%.dsmap" -copyright "GPG 2002" -title "%map_cs%" -author "Johannes Förstner"
+"%tc%\RTC.exe" -source "%tmp%\Bits" -out "%ds%\Maps\%map_cs%.dsmap" -copyright "GPG 2002" -title "%map_cs%" -author "Johannes Förstner"
 if %errorlevel% neq 0 pause
 
 :: Cleanup
